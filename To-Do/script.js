b3c9c991fb452f5aa54b978e57d4d2d98e2a7f1f
Nikhil M <nikhilmahesh27@gmail.com>
2026-03-02 14:53:38 +0530

    To-Do Logics

---
+++ To-Do/script.js
@@ -0,0 +1,38 @@
+const textInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+
+addButton.addEventListener('click', () => {
+    if(textInput.value !== ''){
+        const listItem = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        const updateButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        updateButton.textContent = 'Update';
+
+        deleteButton.addEventListener('click', () => {
+            listItem.remove();
+        })
+
+        updateButton.addEventListener( 'click' , () => {
+            const update = document.createElement('input');
+            const saveButton = document.createElement('button');
+            saveButton.textContent = 'Save';
+            listItem.appendChild(update);
+            listItem.appendChild(saveButton);
+            saveButton.addEventListener('click', () => {
+                if(update.value !== ''){
+                    listItem.textContent = update.value;
+                    listItem.appendChild(deleteButton);
+                    listItem.appendChild(updateButton);
+                }
+            })    
+        })
+
+        listItem.textContent = textInput.value;
+        listItem.appendChild(deleteButton);
+        listItem.appendChild(updateButton);
+        taskList.appendChild(listItem);
+        textInput.value = '';
+    }
+});
